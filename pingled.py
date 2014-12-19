516360c3f9ea16a3ba497482dad2e87506e42079
Ian Dotson <ianm@uw.edu>
2014-12-18 22:40:50 -0800

    Explicitly set gpio direction to DIR_OUT.

--- pingled.py
+++ pingled.py
@@ -13,6 +13,7 @@ def pingloop():
   sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
   sock.bind(('', 0))
   led  = mraa.Gpio(ledpin)
+  led.dir(mraa.DIR_OUT)
   while True :
     data = sock.recv(2)
     toggleled(led)
